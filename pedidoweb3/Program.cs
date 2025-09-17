88d44d2daea1a7c01b8d10ca12b54adaf4762196
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-09-16 20:55:57 -0400

    edicion_program_y_creacion_migracion_SQl

--- pedidoweb3/Program.cs
+++ pedidoweb3/Program.cs
@@ -1,15 +1,35 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using pedidoweb3.Data;
+using pedidoweb3.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PedidosDb")));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
+
+builder.Services.AddAuthorization();
+
+// Password hasher
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,10 +38,33 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+// las rutas
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//inicial (admin + productos demo opcional)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
+    if (!db.Users.Any())
+    {
+        var hasher = scope.ServiceProvider.GetRequiredService<IPasswordHasher<User>>();
+        var admin = new User { Nombre = "Admin", Email = "admin@pedidos.local", Rol = "admin" };
+        admin.PasswordHash = hasher.HashPassword(admin, "Admin123!");
+        db.Users.Add(admin);
+
+        db.Products.AddRange(
+            new Product { Nombre = "Cafe", Categoria = "Bebidas", Precio = 30m, Stock = 50 },
+            new Product { Nombre = "Azúcar", Categoria = "Abarrotes", Precio = 15m, Stock = 200 }
+        );
+        db.SaveChanges();
+    }
+}
 
 app.Run();
